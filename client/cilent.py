49b04b858bb195e1256656130fdce757eb26d12c
Sweetnow <zhangjun990222@qq.com>
2018-07-14 14:28:47 +0800

    server is ok

---
+++ client/cilent.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+import socket
+import queue
+import threading as th
+import copy
+import random
+import time
+
+# 通信线程tcp每次接受的字节数
+tcp_bit = 1024
+# 通信编码
+tcp_coding = "utf-8"
+
+
+def client_main(server_addr="127.0.0.1", server_port=8999):
+    state = {"turn": 0, "flag": 0, "player_0": {"used": 0, "remain": 0, "score": 0}, "player_1": {
+        "used": 0, "remain": 0, "score": 0}}
+    control_dct = {"state": th.Lock(), "next_turn": th.Event()}
+    msg_queue = queue.Queue(1)
+    thread_game = th.Thread(target=game, args=(state, control_dct, msg_queue))
+    thread_game.start()
+    client = socket.socket()
+    try:
+        client.connect((server_addr, server_port))
+    except:
+        print("Error: cannot connect with server")
+        # 处理结束
+        return
+    thread_recv = th.Thread(
+        target=client_recv, args=(client, state, control_dct))
+    thread_send = th.Thread(target=client_send, args=(
+        client, control_dct, msg_queue))
+    thread_recv.start()
+    thread_send.start()
+    thread_game.join()
+    thread_recv.join()
+    thread_send.join()
+
+
+def client_recv(sock, state, control_dct):
+    while True:
+        data = b''
+        while True:
+            buffer = sock.recv(tcp_bit)
+            if buffer == b'':  # server 断开连接
+                pass
+            elif len(buffer) < tcp_bit:
+                data += buffer
+                break
+            else:
+                data += buffer
+        if data == b'':
+            pass  # server 断开连接
+        else:
+            # 数据解析
+            try:
+                msg_recv = json.loads(data.decode(tcp_coding))
+            except:
+                print("Error: cannot load message from server")
+            # 数据解析
+            control_dct["state"].acquire()
+            try:
+                state["turn"] = msg_recv["turn"]
+                state["flag"] = msg_recv["flag"]
+                state["player_0"] = msg_recv["player_0"]
+                state["player_1"] = msg_recv["player_1"]
+            except:
+                print("Error: incomplete message")
+            control_dct["state"].release()
+            control_dct["next_turn"].set()
+
+
+def client_send(sock, control_dct, msg_queue):
+    while True:
+        msg = msg_queue.get()
+        try:
+            sock.sendall(bytes(json.dumps(msg), encoding=tcp_coding))
+        except:
+            print("Error: cannot send message to server")
+
+
+def game(__state, control_dct, msg_queue):
+    while True:
+        control_dct["next_turn"].wait()
+        control_dct["next_turn"].clear()
+        control_dct["state"].acquire()
+        state = copy.deepcopy(__state)
+        control_dct["state"].release()
+
+        p = random.random()
+        used = 0
+        if p < 0.5:
+            used = random.randint(0, 1000)
+        elif p < 1:
+            sleep_time = random.uniform(0.5, 10)
+            time.sleep(sleep_time)
+            used = random.randint(0, 1000)
+        else:
+            while True:
+                pass
+        msg = {"turn": state["turn"]+1, "used": used}
+        msg_queue.put(msg)
+
+
+def main():
+    client_main()
+
+
+if __name__ == "__main__":
+    main()
--- server/server.py
+++ server/server.py
@@ -12,23 +12,24 @@ turn_time = 1
 # 计时线程timer每次睡眠时长(s)
 timer_sleep = 0.05
 # 通信线程tcp每次睡眠时长(s)
-tcp_sleep = 0.02
+tcp_sleep = 0.005
 # 通信线程tcp每次接受的字节数
 tcp_bit = 1024
 # 通信编码
 tcp_coding = "utf-8"
 # 起始筹码数
 game_coin = 1000
-
+# 最大回合数
+max_turn = 50
 turn = 0
 
 
-def server_main(server_addr="127.0.0.1", server_port=8888):
+def server_main(server_addr="127.0.0.1", server_port=8999):
     server = socket.socket()
     server.bind((server_addr, server_port))
     server.listen(2)
     count = 0
-    control_dct = {"start": th.Event(), "send": th.Event(),
+    control_dct = {"start": th.Event(), "send": th.Event(), "stop": th.Event(),
                    0: {"next": th.Lock(), "recv": th.Event(), "send": th.Event()},
                    1: {"next": th.Lock(), "recv": th.Event(), "send": th.Event()}}
     game_queue = {0: {"recv": queue.Queue(1), "send": queue.Queue(1)},
@@ -38,30 +39,51 @@ def server_main(server_addr="127.0.0.1", server_port=8888):
     thread_timer = th.Thread(target=timer, args=(control_dct,))
     thread_timer.start()
     # 游戏处理进程
-    thread_game = th.Thread(target=game, args=(game_queue,))
+    thread_game = th.Thread(target=game, args=(game_queue, control_dct))
     thread_game.start()
-
+    thread_tcp = []
     print("Waiting for connection ......")
     while count < 2:
         sock, addr = server.accept()
         thread = th.Thread(target=tcp, args=(
             sock, control_dct, count, game_queue[count]))
         thread.start()
+        thread_tcp.append(thread)
         count += 1
         print("player_%d is ready. Address: %s" % (count-1, addr))
     print("Game starts")
-    time.sleep(0.05)
+    control_dct[0]["send"].wait()
+    control_dct[1]["send"].wait()
+    control_dct[0]["send"].clear()
+    control_dct[1]["send"].clear()
     control_dct["start"].set()
+    thread_game.join()
+    thread_timer.join()
+    for i in thread_tcp:
+        i.join()
+
 
 def tcp(sock, control_dct, number, this_queue):
-    '''
+    ''' 
     sock - 与client的套接字
     control_dct - 存储多线程控制Event, Lock的字典
     number - client标号
     this_queue - 与game线程信息交换
+
+    初始化客户端
+    发送数据
     '''
+    msg_send = this_queue["send"].get()
+    try:
+        sock.sendall(bytes(json.dumps(msg_send), encoding=tcp_coding))
+    except:
+        print("Error: cannot send message to player_%d" % (number,))
+    # 发送数据
+    control_dct[number]["send"].set()
+
     control_dct["start"].wait()
     sock.setblocking(False)
+
     while True:
         data = b''
         msg_recv = {}
@@ -69,9 +91,20 @@ def tcp(sock, control_dct, number, this_queue):
         control_dct[number]["next"].acquire()
         control_dct[number]["next"].release()
 
-        while not control_dct["send"].isSet:
+        if control_dct["stop"].isSet():
+            this_queue["recv"].put(msg_recv)
+            try:
+                sock.sendall(bytes(json.dumps(msg_recv), encoding=tcp_coding))
+            finally:
+                sock.close()
+                print("tcp %d closes" % (number,))
+                return
+        while not control_dct["send"].isSet():
             while True:
-                buffer = sock.recv(tcp_bit)
+                try:
+                    buffer = sock.recv(tcp_bit)
+                except:
+                    buffer = b''
                 if buffer == b'':
                     break
                 elif len(buffer) < tcp_bit:
@@ -86,8 +119,8 @@ def tcp(sock, control_dct, number, this_queue):
                 try:
                     msg_recv = json.loads(data.decode(tcp_coding))
                 except:
-                    print("Error: cannot load message from player_%d" %
-                          (number,))
+                    print("******Error: cannot load message from player_%d****** %s" %
+                          (number,data))
                 # 数据解析
                 control_dct[number]["recv"].set()
                 break
@@ -104,20 +137,26 @@ def tcp(sock, control_dct, number, this_queue):
 
 
 def timer(control_dct):
+    global turn
     control_dct["start"].wait()
+    control_dct[0]["next"].acquire()
+    control_dct[1]["next"].acquire()
     while True:
-        # 初始化
-        turn += 1  # 起始回合数是1
+        turn += 1
+        if turn > max_turn:
+            control_dct["stop"].set()
         for number in range(2):
             control_dct[number]["recv"].clear()
             control_dct[number]["send"].clear()
         control_dct["send"].clear()
-        start_time=time.time()
+        start_time = time.time()
         control_dct[0]["next"].release()
         control_dct[1]["next"].release()
+        if control_dct["stop"].isSet():
+            return
         while time.time()-start_time < turn_time:
             time.sleep(timer_sleep)
-            if control_dct[0]["recv"].isSet and control_dct[1]["recv"].isSet:
+            if control_dct[0]["recv"].isSet() and control_dct[1]["recv"].isSet():
                 break
 
         # 要求通信线程发送信息并进入下一回合（等待信息发送完成）
@@ -128,7 +167,7 @@ def timer(control_dct):
         control_dct[1]["send"].wait()
 
 
-def game(all_queue):
+def game(all_queue, control_dct):
     '''
     游戏逻辑：新回合->下注->计分->返回结果->下一回合
     recv格式：
@@ -144,45 +183,67 @@ def game(all_queue):
         remain 剩余筹码，如出现错误以此校准
         score 得分
     '''
-    status={0: {"remain": game_coin, "score": 0},
-            1: {"remain": game_coin, "score": 0}}
+    state = {0: {"remain": game_coin, "score": 0},
+             1: {"remain": game_coin, "score": 0}}
+    # 初始化客户端
+    send_0 = {"turn": turn, "flag": 0, "player_0": {"used": 0, "remain": state[0]["remain"], "score": state[0]["score"]}, "player_1": {
+        "used": 0, "remain": state[1]["remain"], "score": state[1]["score"]}}
+    all_queue[0]["send"].put(send_0)
+    send_1 = {"turn": turn, "flag": 1, "player_0": {"used": 0, "remain": state[0]["remain"], "score": state[0]["score"]}, "player_1": {
+        "used": 0, "remain": state[1]["remain"], "score": state[1]["score"]}}
+    all_queue[1]["send"].put(send_1)
     while True:
-        recv={0: {}, 1: {}}
-        recv[0]=all_queue[0]["recv"].get()
-        recv[1]=all_queue[1]["recv"].get()
-        used={0: 0, 1: 0}
+        recv = {0: {}, 1: {}}
+        recv[0] = all_queue[0]["recv"].get()
+        recv[1] = all_queue[1]["recv"].get()
+        print(recv[0])
+        print(recv[1])
+        if control_dct["stop"].isSet():
+            for i in range(2):
+                print("player_%d remains %d coins, Score: %d" %
+                      (i, state[i]["remain"], state[i]["score"]))
+            if state[0]["score"] != state[1]["score"]:
+                print("Winner is player_%d" %
+                      (0 if state[0]["score"] > state[1]["score"] else 1,))
+            else:
+                print("No Winner")
+            return
+        used = {0: 0, 1: 0}
         # 合法性检测
         for i in range(2):
-            if len(recv[i]) > 0 and recv[i]["turn"] == turn and recv["used"] <= status[i]["remain"] and recv["used"] >= 0:
-                used[i]=recv["used"]
-                status[i]["remain"] -= used[i]
+            if len(recv[i]) > 0 and recv[i]["turn"] == turn and recv[i]["used"] <= state[i]["remain"] and recv[i]["used"] >= 0:
+                used[i] = recv[i]["used"]
+                state[i]["remain"] -= used[i]
             else:
                 # debug
-                if len(recv[i]) > 0:
+                if len(recv[i]) == 0:
                     pass
                 elif recv[i]["turn"] != turn:
                     print("Error: player_%d's turn is %d, server's turn is %d" % (
                         i, recv[i]["turn"], turn))
-                elif recv["used"] > status[i]["remain"]:
+                elif recv[i]["used"] > state[i]["remain"]:
                     print("player_%d used too many coins" % (i,))
-                elif recv["used"] < 0:
-                    print("stupid player_%d used %d coin" % (i, recv["used"]))
+                elif recv[i]["used"] < 0:
+                    print("stupid player_%d used %d coin" % (i, recv[i]["used"]))
 
         if used[0] > used[1]:
-            status[0]["score"] += 2
+            state[0]["score"] += 2
         elif used[0] == used[1]:
-            status[0]["score"] += 1
-            status[1]["score"] += 1
+            state[0]["score"] += 1
+            state[1]["score"] += 1
         else:
-            status[1]["score"] += 2
-        send_0={"turn": turn, "flag": 0, "player_0": {"used": used[0], "remain": status[0]["remain"], "score": status[0]["score"]}, "player_1": {
-            "used": used[1], "remain": status[1]["remain"], "score": status[1]["score"]}}
+            state[1]["score"] += 2
+        print("turn: "+str(turn))
+        for i in range(2):
+            print("player_%d remains %d coins, Score: %d" %
+                  (i, state[i]["remain"], state[i]["score"]))
+        print("")
+        send_0 = {"turn": turn, "flag": 0, "player_0": {"used": used[0], "remain": state[0]["remain"], "score": state[0]["score"]}, "player_1": {
+            "used": used[1], "remain": state[1]["remain"], "score": state[1]["score"]}}
         all_queue[0]["send"].put(send_0)
-        send_1={"turn": turn, "flag": 1, "player_0": {"used": used[0], "remain": status[0]["remain"], "score": status[0]["score"]}, "player_1": {
-            "used": used[1], "remain": status[1]["remain"], "score": status[1]["score"]}}
+        send_1 = {"turn": turn, "flag": 1, "player_0": {"used": used[0], "remain": state[0]["remain"], "score": state[0]["score"]}, "player_1": {
+            "used": used[1], "remain": state[1]["remain"], "score": state[1]["score"]}}
         all_queue[1]["send"].put(send_1)
-
-
 
 
 def main():
